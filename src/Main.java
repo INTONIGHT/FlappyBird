84489219766b2a3e5920de04205753e984cd0e78
INTONIGHT <tyler.throop@gmail.com>
2025-08-12 10:43:49 -0700

    adding the background to the window

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int boardWidth = 360;
+		int boardHeight = 640;
+		
+		JFrame frame = new JFrame("FlappyBird");
+		
+		frame.setSize(boardWidth, boardHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		FlappyBird flappyBird = new FlappyBird();
+		frame.add(flappyBird);
+		frame.pack();
+		
+		frame.setVisible(true);
+	}
+
+}
