21b30e3b55cea455690aa57622a0be707c520a2a
INTONIGHT <tyler.throop@gmail.com>
2025-08-13 14:18:55 -0700

    And done

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -24,7 +24,7 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 		int x = birdX;
 		int y = birdY;
 		int width = birdWidth;
-		int heigh = birdHeight;
+		int height = birdHeight;
 		Image img;
 		Bird(Image img){
 			this.img = img;
@@ -59,6 +59,9 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 	
 	Timer gameLoop;
 	Timer placePipesTimer;
+	double score = 0;
+	
+	boolean gameOver = false;
 	
 	FlappyBird(){
 		setPreferredSize(new Dimension(boardWidth,boardHeight));
@@ -96,12 +99,20 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 		//background
 		g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
 		//bird
-		g.drawImage(bird.img,bird.x,bird.y,bird.width,bird.heigh,null);
+		g.drawImage(bird.img,bird.x,bird.y,bird.width,bird.height,null);
 		
 		//pipes
 		for(int i =0; i<pipes.size();i++) {
 			Pipe pipe = pipes.get(i);
 			g.drawImage(pipe.img,pipe.x,pipe.y,pipe.width,pipe.height,null);
+		}
+		//scroe
+		g.setColor(Color.white);
+		g.setFont(new Font("Arial",Font.PLAIN,32));
+		if(gameOver) {
+			g.drawString("Game Over: " + String.valueOf((int) score),10,35 );
+		}else {
+			g.drawString(String.valueOf((int) score), 10, 35);
 		}
 		
 	}
@@ -114,8 +125,19 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 		for(int i =0; i<pipes.size();i++) {
 			Pipe pipe = pipes.get(i);
 			pipe.x += velocityX;
+			if(!pipe.passed && bird.x > pipe.x + pipe.width) {
+				pipe.passed = true;
+				score += 0.5; //2 pipes split score between the pipes
+			}
+			
+			if(collision(bird,pipe)) {
+				gameOver = true;
+			}
 		}
 		
+		if(bird.y > boardHeight) {
+			gameOver = true;
+		}
 	}
 
 	@Override
@@ -123,6 +145,10 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 		// TODO Auto-generated method stub
 		move();
 		repaint();
+		if(gameOver) {
+			placePipesTimer.stop();
+			gameLoop.stop();
+		}
 	}
 
 	@Override
@@ -136,6 +162,16 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 		// TODO Auto-generated method stub
 		if(e.getKeyCode()== KeyEvent.VK_SPACE) {
 			velocityY = - 9;
+			if(gameOver) {
+				//restart the game by resetting the conditions
+				bird.y = birdY;
+				velocityY = 0;
+				pipes.clear();
+				score = 0;
+				gameOver =false;
+				gameLoop.start();
+				placePipesTimer.start();
+			}
 		}
 	}
 
@@ -159,5 +195,12 @@ public class FlappyBird extends JPanel implements ActionListener, KeyListener{
 		Pipe bottomPipe = new Pipe(bottomPipeImg);
 		bottomPipe.y = topPipe.y + pipeHeight + openingSpace;
 		pipes.add(bottomPipe);
+	}
+	
+	public boolean collision(Bird a, Pipe b) {
+		return a.x < b.x + b.width && //as top left corner doesnt reach bs top left corner
+				a.x + a.width > b.x &&//as top right corner passes bs top left corner
+				a.y < b.y + b.height &&//as top left corner doesnt reach bs bottom left corner
+				a.y + a.height > b.y;//as bottom left corner basses bs top left corner
 	}
 }
